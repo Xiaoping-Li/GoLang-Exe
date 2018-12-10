b840830c1b1b7cc835aa4fc77f7c262efbe464da
Xiaoping-Li <narci2008@gmail.com>
2018-12-10 09:49:09 -0800

    Update hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -16,3 +16,20 @@ Sample Output
 Hello, World. 
 Welcome to 30 Days of Code!
 */
+
+package main
+import (
+   "fmt" 
+   "bufio"
+   "os"
+)
+
+
+func main() {
+ //Enter your code here. Read input from STDIN. Print output to STDOUT
+    reader := bufio.NewReader(os.Stdin)
+    text, _ := reader.ReadString('\n')
+
+    fmt.Printf("Hello, World. \n")
+    fmt.Printf(text)
+}
